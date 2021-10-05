6962bd586473a4c36cacbf61c344bea839ad2296
Ray Johnson <changeforgrace@gmail.com>
2021-10-05 08:07:09 -0400

    fixed responsive video component

--- src/Video.js
+++ src/Video.js
@@ -3,16 +3,28 @@ import React from "react";
 function Video() {
 	return (
 		<>
-			<h1>Ron's Best Moments</h1>
-			<iframe
-				title="Ron Swanson Supercut"
-				width="560"
-				height="315"
-				src="https://www.youtube.com/embed/SrLZgP-OR6s"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowfullscreen
-			></iframe>
+			<div className="ui container">
+				<div className="ui grid">
+					<div className="ui row">
+						<div className="sixteen wide column">
+							<div className="ui segment topspace">
+								<h3 className="ui header">Ron's Best Moments</h3>
+							</div>
+						</div>
+						<div className="sixteen wide column">
+							<div className="ui embed">
+								<iframe
+									title="Ron Swanson Supercut"
+									src="https://www.youtube.com/embed/SrLZgP-OR6s"
+									frameborder="0"
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+									allowfullscreen
+								></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
