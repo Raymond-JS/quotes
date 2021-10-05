801c4cdf576dcf57c5fb04913b740ea61a44ced7
Ray Johnson <changeforgrace@gmail.com>
2021-10-05 05:21:43 -0400

    fixed responsive video element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Quote from "./Quote";
+import Video from "./Video";
+import About from "./About";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="ui grid centered container ">
+			<Header />
+			<Switch>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route exact path="/">
+					<Quote />
+				</Route>
+				<Route path="/video">
+					<Video />
+				</Route>
+			</Switch>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
